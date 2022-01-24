e6d006d394a49785ed9ebfe52a8f8856ea412353
Lakita Gaither <lakitagaither@yahoo.com>
2022-01-24 10:01:04 -0600

    File Structure

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header() {
+    return (
+        <h3>This is my header element</h3>
+    )
+}
+
+export default Header;
---
+++ src/components/Nav.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Nav() {
+    return (
+        <h3>This is my Nav element</h3>
+    )
+}
+
+export default Nav;
